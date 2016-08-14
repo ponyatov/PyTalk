21f20783f89c43b312ab7c675a4e36a56964117e
Dmitry Ponyatov <dponyatov@gmail.com>
2016-08-14 10:41:21 +0400

    + about meta
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>\

---
+++ meta.py
@@ -0,0 +1,8 @@
+Title		='PyTalk'
+About		='Python-powered SmallTalk-like object environment'	
+Version		='0.0.0.1 alpha'
+Author		= 'Dmitry Ponyatov <dponyatov@gmail.com>'
+License		='GNU LesserGPL'
+Copyright	='(c) %s , %s'%(Author,License)
+GitHub		='https://github.com/ponyatov/PyTalk'
+
--- py.py
+++ py.py
@@ -1,13 +1,38 @@
 import os,sys,wx
+import meta
 
 app = wx.App()
 
-frmMain = wx.Frame(None,-1,os.getcwd())
+frmMain = wx.Frame(parent=None,id=-1,title=os.getcwd())
 frmMain.SetIcon(wx.Icon('logo64x64.ico'))
-
-
-
+frmMain.Maximize()
 frmMain.Show()
+
+menu = wx.MenuBar()
+frmMain.SetMenuBar(menu)
+
+menuFile = wx.Menu()
+menu.Append(menuFile,'&File')
+
+def OnQuit(e):
+	frmMain.Close()
+menuQuit = menuFile.Append(wx.ID_EXIT,'&Quit')
+frmMain.Bind(wx.EVT_MENU,OnQuit,menuQuit)
+
+menuHelp = wx.Menu()
+menu.Append(menuHelp,'&Help')
+menuAbout = menuHelp.Append(wx.ID_ABOUT,'&About\tF1')
+def OnAbout(e):
+	info = wx.AboutDialogInfo()
+	info.Name = meta.Title
+	info.Version = meta.Version
+	info.Copyright = meta.Copyright
+	info.Description = meta.About
+	info.WebSite = meta.GitHub
+#	info.License  = meta.License
+#	info.Developers = meta.Author
+	wx.AboutBox(info)
+frmMain.Bind(wx.EVT_MENU,OnAbout,menuAbout)
 
 app.MainLoop()
 
